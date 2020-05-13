1e872197fdc3e40709e8cfc46f93311b975f2a1d
李杰 <kory.lee@gmail.com>
2020-05-14 00:46:18 +0800

    upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'space-before-function-paren': 0
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
   }
 }
