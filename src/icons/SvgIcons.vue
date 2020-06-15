61482e25f236257051ecb939337af3ebba22873b
kory <62596617+Kory-lee@users.noreply.github.com>
2020-06-16 00:39:08 +0800

    feat：svg-icon 模块

---
+++ src/icons/SvgIcons.vue
@@ -0,0 +1,41 @@
+<template>
+  <!-- <div>{{ msg }}</div> -->
+  <svg :class="svgClass" aria-hidden="true">
+    <use :xlink:href="name"></use>
+  </svg>
+</template>
+
+<script>
+import { computed } from '@vue/composition-api';
+export default {
+  name: 'svgIcon',
+  props: { iconName: { type: String, required: true }, className: { type: String } },
+  setup(props) {
+    const name = computed(() => `#icon-${props.iconName}`);
+    // `svg-icon ${props.iconName} ${props.className ? props.className : ''}`;
+    const svgClass = computed(() => ['svg-icon', props.iconName, props.className]);
+    return {
+      name,
+      svgClass,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+  // color: #fff;
+  &.small {
+    font-size: $smallFont;
+  }
+  &.medium {
+    font-size: $mediumFont;
+  }
+  &.regular {
+    font-size: $regularFont;
+  }
+}
+</style>
