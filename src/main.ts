c728c9901435adfdcc853de18361ccc8dff96cc9
kory <kory.leejie@gmail.com>
2020-10-23 17:55:27 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import antD from "/@/plugin/ant-design-vue/index";
 import router from "/@/router";
-import "/@/styles/index.less";
 
 const app = createApp(App);
 
-app.use(router).use(antD);
-app.mount("#app");
+app.use(router).use(antD).mount("#app");
