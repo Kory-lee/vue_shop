7389e2476f8a02a6272ccfb7640ef431006feeed
kory <kory.leejie@gmail.com>
2020-10-23 17:55:48 +0800

    delete console.log

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import type { RouteRecordRaw } from "vue-router";
 import { createRouter, createWebHashHistory } from "vue-router";
 import { basicRoutes } from "./routes";
+console.log(basicRoutes);
+
 const router = createRouter({
   history: createWebHashHistory(),
   routes: basicRoutes as RouteRecordRaw[],
