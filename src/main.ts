0092f19ed536534a3065c6d4bf58de1406575f1c
kory <62596617+Kory-lee@users.noreply.github.com>
2020-11-24 22:57:31 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from "./App";
 import antD from "./plugins/antD";
 import router from "./router";
 import store from "./store";
-import "./styles/index.css";
+import "./styles/index.less";
 const app = createApp(App);
 
 app.use(store).use(router).use(antD).mount("#app");
