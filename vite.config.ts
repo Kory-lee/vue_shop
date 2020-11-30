a9749fc5f132b76f7a2af82bef7a3092b8543be5
kory <62596617+Kory-lee@users.noreply.github.com>
2020-12-01 00:05:37 +0800

    fix: @ant-design/icon-vue On-demand import

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { resolve } from "path";
-import { UserConfig } from "vite";
+import { resolve } from 'path';
+import { UserConfig } from 'vite';
 const pathResolve = (dir: string): string => resolve(__dirname, dir);
 
 const config: UserConfig = {
@@ -8,6 +8,8 @@ const config: UserConfig = {
    * @default 3000
    */
   // port: VITE_PORT,
-  alias: { "/@/": pathResolve("src") },
+  alias: { '/@/': pathResolve('src') },
+  // 解决@ant-design无法单独引入，引入全部导致页面卡死的困境
+  optimizeDeps: { include: ['@ant-design/icons-vue'] },
 };
 export default config;
