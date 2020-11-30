eee5911f2ffa4c356d6425e34da3e2bf140863df
kory <kory.leejie@gmail.com>
2020-11-30 16:51:03 +0800

    singleQuote

--- src/views/sys/exception/index.vue
+++ src/views/sys/exception/index.vue
@@ -1,10 +1,12 @@
 <script lang="ts">
-import { computed, defineComponent, h, ref, unref } from "vue";
-import { Result, Button } from "ant-design-vue";
-import { ExceptionEnum } from "/@/enums/exceptionEnums";
-import { useI18n } from "/@/plugins/i18n";
-import { useRoute } from "vue-router";
-import NoDataImg from "/@/assets/img/exception/no-data.png";
+import { computed, defineComponent, h } from 'vue';
+import { Result, Button } from 'ant-design-vue';
+import { ExceptionEnum } from '/@/enums/exceptionEnum';
+import { useI18n } from '/@/plugins/i18n';
+import { useRoute } from 'vue-router';
+import { useRefresh } from '/@/hooks/web/usePage';
+import NoDataImg from '/@/assets/img/exception/no-data.png';
+import netWorkImg from '/@/assets/img/exception/net-work.png';
 interface MapValue {
   title: string;
   status?: string;
@@ -15,7 +17,7 @@ interface MapValue {
 }
 
 export default defineComponent({
-  name: "ErrorPage",
+  name: 'ErrorPage',
   props: {
     status: { type: Number, default: ExceptionEnum.PAGE_NOT_FOUND },
     title: { type: String },
@@ -23,59 +25,68 @@ export default defineComponent({
     full: { type: Boolean, default: false },
   },
   setup(props) {
-    const statusMapRef = ref(new Map<string | number, MapValue>());
-    const { t } = useI18n("sys.exception");
-    const { query } = useRoute();
-    const getStatus = computed(() => {
-      const { status } = query;
-      // return Number(status) || props.status;
-      return 10100;
-    });
-    const getMapValue = computed(() =>
-      unref(statusMapRef).get(unref(getStatus))
-    );
+    const statusMapRef = new Map<string | number, MapValue>();
+    const { t } = useI18n('sys.exception'),
+      route = useRoute(),
+      refresh = useRefresh();
 
-    const backLoginI18n = t("backLogin"),
-      backHomeI18n = t("backHome");
-    unref(statusMapRef).set(ExceptionEnum.PAGE_NOT_ACCESS, {
-      title: "403",
-      status: `${ExceptionEnum.PAGE_NOT_ACCESS}`,
-      subTitle: t("subTitle403"),
-      btnText: props.full ? backLoginI18n : backHomeI18n,
-    });
-    unref(statusMapRef).set(ExceptionEnum.PAGE_NOT_FOUND, {
-      title: "404",
-      status: `${ExceptionEnum.PAGE_NOT_FOUND}`,
-      subTitle: t("subTitle404"),
-      btnText: props.full ? backLoginI18n : backHomeI18n,
-    });
-    unref(statusMapRef).set(ExceptionEnum.PAGE_NOT_DATA, {
-      title: t("noDataTitle"),
-      subTitle: "",
-      btnText: t("redo"),
-      // handler:()=>redo(),
-      icon: NoDataImg,
-    });
+    const getStatus = computed(() => {
+        const status = Number(route.query.status);
+        if ([403, 404, 500, 10100, 10000].includes(status)) return status;
+        return props.status;
+      }),
+      getMapValue = computed(() => statusMapRef.get(getStatus.value));
+
+    const backLoginI18n = t('backLogin'),
+      backHomeI18n = t('backHome');
+    statusMapRef
+      .set(ExceptionEnum.PAGE_NOT_ACCESS, {
+        title: '403',
+        status: `${ExceptionEnum.PAGE_NOT_ACCESS}`,
+        subTitle: t('subTitle403'),
+        btnText: props.full ? backLoginI18n : backHomeI18n,
+      })
+      .set(ExceptionEnum.PAGE_NOT_FOUND, {
+        title: '404',
+        status: `${ExceptionEnum.PAGE_NOT_FOUND}`,
+        subTitle: t('subTitle404'),
+        btnText: props.full ? backLoginI18n : backHomeI18n,
+      })
+      .set(ExceptionEnum.ERROR, {
+        title: '500',
+        status: `${ExceptionEnum.ERROR}`,
+        subTitle: t('subTitle500'),
+        btnText: backHomeI18n,
+      })
+      .set(ExceptionEnum.PAGE_NOT_DATA, {
+        title: t('noDataTitle'),
+        subTitle: '',
+        btnText: t('redo'),
+        handler: () => refresh(),
+        icon: NoDataImg,
+      })
+      .set(ExceptionEnum.NET_WORK_ERROR, {
+        title: t('networkErrorTitle'),
+        subTitle: t('networkErrorSubTitle'),
+        btnText: t('redo'),
+        icon: netWorkImg,
+        handler: () => refresh(),
+      });
 
     return () => {
-      const { title, subTitle, btnText, status, handler, icon } =
-        unref(getMapValue) || {};
+      const { title, subTitle, btnText, status, handler, icon } = getMapValue.value || {};
       return h(
         Result,
         {
-          class: "exception",
+          class: 'exception',
           status: status as any,
           title: props.title || title,
           subTitle: props.subTitle || subTitle,
+          onClick: handler,
         },
         {
-          extra: () =>
-            h(
-              Button,
-              { type: "primary", onClick: handler },
-              { default: () => btnText }
-            ),
-          icon: () => (icon ? h("img", { src: icon }) : null),
+          extra: () => h(Button, { type: 'primary', onClick: handler }, { default: () => btnText }),
+          icon: () => (icon ? h('img', { src: icon }) : null),
         }
       );
     };
@@ -83,4 +94,10 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.exception {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
