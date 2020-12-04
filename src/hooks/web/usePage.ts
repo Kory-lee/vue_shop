a1daec5730150055684ebb979d6c631dda6fcb01
kory <kory.leejie@gmail.com>
2020-12-04 10:54:09 +0800

    wip: create usePage hooks

--- src/hooks/web/usePage.ts
+++ src/hooks/web/usePage.ts
@@ -1,5 +1,24 @@
 import { unref } from 'vue';
-import { useRouter } from 'vue-router';
+import { RouteLocationRaw, useRouter } from 'vue-router';
+import { PageEnum } from '/@/enums/PageEnum';
+import { isString } from '/@/utils/is';
+export type RouteLocationRawEx = Omit<RouteLocationRaw, 'path'> & { path: PageEnum };
+function handleError(e: Error) {
+  console.error(e);
+  // 为了大于打开时候设置的100延时防止闪动
+  setTimeout(() => {}, 101);
+}
+export function useGo() {
+  const { push, replace } = useRouter();
+  return (opt: PageEnum | RouteLocationRawEx | string = PageEnum.BASE_HOME, isReplace = false) => {
+    if (!opt) return;
+    if (isString(opt)) isReplace ? replace(opt).catch(handleError) : push(opt).catch(handleError);
+    else {
+      const o = opt as RouteLocationRaw;
+      isReplace ? replace(o).catch(handleError) : push(o).catch(handleError);
+    }
+  };
+}
 
 export const useRefresh = () => {
   const { push, currentRoute } = useRouter();
