5321f61e288d58c1b3fe6a8a19484927a1ae50bb
kory <kory.leejie@gmail.com>
2020-12-05 11:15:36 +0800

    📍feat:create  locale picker

---
+++ src/components/DropDown/index.vue
@@ -0,0 +1,15 @@
+
+<script lang="ts">
+import Dropdown from 'ant-design-vue/lib/dropdown/dropdown';
+import { defineComponent, h } from 'vue';
+
+export default defineComponent({
+  name: 'Dropdown',
+  props:{},
+  setup() {
+    return ()=>h(Dropdown, )
+  },
+});
+</script>
+
+<style></style>
