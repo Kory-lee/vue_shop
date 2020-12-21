dc4a44674374a526abae35b6985fe8d33249545d
kory <kory.leejie@gmail.com>
2020-12-21 19:37:29 +0800

    wip(logo): logo component

--- src/components/Application/Logo.vue
+++ src/components/Application/Logo.vue
@@ -1,21 +1,30 @@
 <template>
-  <div class="anticon" :class="[theme]">
+  <div class="anticon" :class="[theme, prefixCls]" @click="handleGoHome">
     <img src="/@/assets/img/logo.png" alt="" />
-    <div class="ml-2 ellipsis" v-show="showTitle"></div>
+    <div class="ml-2 ellipsis" :class="`${prefixCls}__title`" v-show="showTitle"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
-
+import { defineComponent, inject, PropType } from 'vue';
+import { getPrefixCls as customizePrefixCls } from '/@/components/Application';
+import { PageEnum } from '/@/enums/pageEnum';
+import { useGo } from '/@/hooks/web/usePage';
+// import {getCollapsedShowTitle}
 export default defineComponent({
   name: 'Logo',
   props: {
     theme: { type: String as PropType<'light' | 'dark'> },
     showTitle: { type: Boolean, default: true },
   },
-  setup() {},
+  setup() {
+    const go = useGo();
+    const handleGoHome = () => go(PageEnum.BASE_HOME);
+    const getPrefixCls = inject('getPrefixCls', customizePrefixCls);
+    const prefixCls = getPrefixCls('app-logo');
+    return { prefixCls, handleGoHome };
+  },
 });
 </script>
 
-<style lang="less"></style>
+<style lang="less" scoped></style>
