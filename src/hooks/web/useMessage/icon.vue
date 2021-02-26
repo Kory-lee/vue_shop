87bdc74759977d567585f0bd8f3c1c6f5b85968a
kory <kory.leejie@gmail.com>
2021-02-26 18:12:48 +0800

    feat(createMessage): message & modal secondary packaging

---
+++ src/hooks/web/useMessage/icon.vue
@@ -0,0 +1,38 @@
+<script lang="tsx">
+  import { InfoCircleFilled, CheckCircleFilled, CloseCircleFilled } from '@ant-design/icons-vue';
+  import { defineComponent, PropType } from 'vue';
+  const getIcons = {
+    warning: InfoCircleFilled,
+    success: CheckCircleFilled,
+    info: InfoCircleFilled,
+    error: CloseCircleFilled,
+  };
+  export type iconsType = keyof typeof getIcons;
+  export default defineComponent({
+    name: 'ModalIcon',
+    // components: { InfoCircleFilled, CheckCircleFilled },
+    props: { type: { type: String as PropType<iconsType>, default: 'info' } },
+    setup(props) {
+      const MIcon = getIcons[props.type];
+      return () => <MIcon class={`modal-icon-${props.type}`} />;
+    },
+  });
+</script>
+
+<style lang="less">
+  .modal-icon-warning {
+    color: @warning-color !important;
+  }
+
+  .modal-icon-success {
+    color: @success-color !important;
+  }
+
+  .modal-icon-error {
+    color: @error-color !important;
+  }
+
+  .modal-icon-info {
+    color: @primary-color !important;
+  }
+</style>
