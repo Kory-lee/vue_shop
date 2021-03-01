c9ae33dac0d723a5d6b850f37140ed8b34bc652d
kory <kory.leejie@gmail.com>
2021-03-01 17:53:58 +0800

    delete

--- src/api/common.js
+++
@@ -1,9 +0,0 @@
-import service from "@utils/request";
-// 获取token
-export function ImageToken(data) {
-  return service.request({
-    method: "post",
-    url: "/uploadImgToken/",
-    data,
-  });
-}
