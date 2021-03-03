9fd434138b0aef5170df225f3cbb52401857493d
kory <kory.leejie@gmail.com>
2021-03-03 17:23:53 +0800

    fix: mock bug

--- build/utils.ts
+++ build/utils.ts
@@ -20,10 +20,10 @@ export function wrapperEnv(envConfig: any): ViteEnv {
   for (const envName of Object.keys(envConfig)) {
     let realName = envConfig[envName].replace(/\\n/g, '\n');
     realName = realName === 'true' ? true : realName === 'false' ? false : realName;
-    if (envName === 'VITE_PORT') realName === Number(realName);
+    if (envName === 'VITE_PORT') realName = Number(realName);
     else if (envName === 'VITE_PROXY') {
       try {
-        realName === JSON.parse(realName);
+        realName = JSON.parse(realName);
       } catch (e) {}
     }
     ret[envName] = realName;
