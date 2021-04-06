9b02fc01a84e9793f9847a3d21ac16c58805dd4d
Kory-lee <kory.lee@qq.com>
2021-04-06 14:23:43 +0800

    chore: close explicit-any

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,6 @@ module.exports = defineConfig({
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 });
