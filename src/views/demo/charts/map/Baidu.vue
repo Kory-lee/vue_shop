2f08f03d608ffd8aafbd528cd2f334f999c16b25
kory <kory.lee@qq.com>
2022-03-20 20:29:24 +0800

    chore: update deps

--- src/views/demo/charts/map/Baidu.vue
+++ src/views/demo/charts/map/Baidu.vue
@@ -1,14 +1,13 @@
 <template>
   <div ref="wrapRef" :style="{ height, width }"></div>
 </template>
-
 <script lang="ts">
-  import { defineComponent, nextTick, onMounted, ref, unref } from 'vue';
+  import { defineComponent, ref, nextTick, unref, onMounted } from 'vue';
+
   import { useScript } from '/@/hooks/web/useScript';
 
   const BAI_DU_MAP_URL =
-    'https://api.map.baidu.com/api?v=1.0&type=webgl&ak=ibzAyAif5MnYFqi2VVQN0mpPCGwV7MTt';
-
+    'https://api.map.baidu.com/getscript?v=3.0&ak=OaBvYmKX3pjF7YFUFeeBCeGdy9Zp7xB2&services=&t=20210201100830&s=1';
   export default defineComponent({
     name: 'BaiduMap',
     props: {
@@ -29,8 +28,8 @@
         await toPromise();
         await nextTick();
         const wrapEl = unref(wrapRef);
+        if (!wrapEl) return;
         const BMap = (window as any).BMap;
-
         const map = new BMap.Map(wrapEl);
         const point = new BMap.Point(116.404, 39.915);
         map.centerAndZoom(point, 15);
@@ -40,6 +39,7 @@
       onMounted(() => {
         initMap();
       });
+
       return { wrapRef };
     },
   });
