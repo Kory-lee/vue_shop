f7c16172d607606f2f80cedc4fc65b0699275bc4
kory <kory.lee@qq.com>
2022-04-05 06:43:19 +0800

    chore: build

--- build/vite/plugins/html.ts
+++ build/vite/plugins/html.ts
@@ -1,18 +1,20 @@
-import html from 'vite-plugin-html';
+import type { PluginOption } from 'vite';
+
+import { createHtmlPlugin } from 'vite-plugin-html';
 import pkg from '../../../package.json';
 import { GLOBAL_CONFIG_FILE_NAME } from '../../constant';
 
 export function configHtmlPlugin(env: ViteEnv, isBuild: boolean) {
-  const { VITE_GLOBAL_APP_TITLE, VITE_PUBLIC_PATH } = env,
-    path = VITE_PUBLIC_PATH.endsWith('/') ? `${VITE_PUBLIC_PATH}` : `${VITE_PUBLIC_PATH}/`;
+  const { VITE_GLOBAL_APP_TITLE, VITE_PUBLIC_PATH } = env;
+  const path = VITE_PUBLIC_PATH.endsWith('/') ? VITE_PUBLIC_PATH : `${VITE_PUBLIC_PATH}/`;
 
   const getAppConfigSrc = () =>
     `${path || '/'}${GLOBAL_CONFIG_FILE_NAME}?v=${pkg.version}-${new Date().getTime()}`;
-  return html({
+  return createHtmlPlugin({
     minify: isBuild,
     inject: {
       data: { title: VITE_GLOBAL_APP_TITLE },
-      tags: isBuild ? [{ tag: 'script', attrs: { src: getAppConfigSrc() } }] : [],
+      tags: isBuild ? [{ tag: 'script', attrs: { src: getAppConfigSrc()} }] : [],
     },
-  });
+  }) as PluginOption[];
 }
